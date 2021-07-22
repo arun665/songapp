9c2bc320e2d3c2f50ba03dac21532a50dad842c9
arun sharma <arunsharmamoh@mgmail.com>
2021-07-22 17:00:09 +0530

    sdfsdf

--- src/App.js
+++ src/App.js
@@ -2,11 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import vail from './vail.mp4';
 import gshit from './gshit.mp3';
+import bad from './bad.mp3';
+import song295 from './295.mp3';
+import Brown from './Brown.mp3';
+import us from './us.mp3';
+import Legend from './Legend.mp3';
+import Arrogant from './apdhillon/Arrogant.mp3';
+import Droptop from './apdhillon/Droptop.mp3';
+import Excuses from './apdhillon/Excuses.mp3';
+import Farrar from './apdhillon/Faraar.mp3';
+import Goat from './apdhillon/Goat.mp3';
+import Insane from './apdhillon/Insane.mp3';
+import Majhail from './apdhillon/Majhail.mp3';
+import Wanted from './apdhillon/Wanted.mp3';
+import Takeover from './apdhillon/Takeover.mp3';
+import Goli from './karan/Goli.mp3';
+import Jhanjra from './karan/Jhanjra.mp3';
+import Kaali from './karan/Kaali.mp3';
+import Mitran from './karan/Mitran.mp3';
+import Phulkari from './karan/Phulkari.mp3';
+
 function App() {
   return (
     <div className="App">
       
-
+{/*
 
       <video controls>
   <source src={vail} type="video/mp4" />
@@ -14,12 +34,464 @@ function App() {
   Your browser does not support the video tag.
 </video>
 
+*/}
 
-<audio controls>
-  <source src={gshit} type="audio/ogg"/>
+
+
+<div class="container">
+  <h2> Welcome arun  </h2>
+  <div id="accordion">
+    <div class="card">
+      <div class="card-header">
+        <a class="card-link" data-toggle="collapse" href="#collapseOne">
+          Sidhu MooseWala
+        </a>
+      </div>
+      <div id="collapseOne" class="collapse show" data-parent="#accordion">
+        <div class="card-body">
+            <div className="row">
+
+              <div className="col-sm-6 col-md-6 col-12">
+              <h5>G-Shit</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+      
+              <audio controls>
+  <source src={gshit} />
   
   Your browser does not support the audio element.
 </audio>
+</div>
+
+            </div>
+
+            <hr></hr>
+
+
+            <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>Bad</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={bad} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+
+            <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>295</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={song295} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+            <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>Brown Shortie</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Brown} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+            
+            <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>us</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={us} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+            
+            <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>Legend</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Legend} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">
+        <a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo">
+        AP Dhillon
+      </a>
+      </div>
+      <div id="collapseTwo" class="collapse" data-parent="#accordion">
+        <div class="card-body">
+   
+        <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>Arrogant</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Arrogant} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+
+
+            <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>Droptop</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Droptop} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+
+
+            <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>Excuses</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Excuses} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+
+            
+        <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>Faraar</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Farrar} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+
+
+            <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>Goat</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Goat} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+     
+     
+            <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>Insane</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Insane} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+
+            <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>Majhail</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Majhail} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+     
+     
+            <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>Takeover</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Takeover} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+     
+
+            <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>Wanted</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Wanted} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+
+        </div>
+      </div>
+    </div>
+
+
+    <div class="card">
+      <div class="card-header">
+        <a class="collapsed card-link" data-toggle="collapse" href="#collapseThree">
+          Karan Randhawa
+        </a>
+      </div>
+      <div id="collapseThree" class="collapse" data-parent="#accordion">
+        <div class="card-body">
+  
+  
+        <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5> Goli </h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Goli} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+  
+            <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5> Jhanjra </h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Jhanjra} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+
+              
+        <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5> Kaali </h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Kaali} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+  
+            <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5> Mitran</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Mitran} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+              
+        <div className="row">
+
+<div className="col-sm-6 col-md-6 col-12">
+<h5>  Phulkari</h5>
+</div>
+<div className="col-sm-6 col-md-6 col-12">
+
+<audio controls>
+<source src={Phulkari} />
+
+Your browser does not support the audio element.
+</audio>
+</div>
+
+</div>
+
+
+            <hr></hr>
+
+
+  
+  
+  
+  
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
 
     </div>
   );

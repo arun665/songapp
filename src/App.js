3602c135aab9bffba3925f07a7cb14b6aaa95ecf
arun sharma <arunsharmamoh@mgmail.com>
2021-07-22 14:59:31 +0530

    sdfsdf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import vail from './vail.mp4';
+import gshit from './gshit.mp3';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+
+      <video controls>
+  <source src={vail} type="video/mp4" />
+  
+  Your browser does not support the video tag.
+</video>
+
+
+<audio controls>
+  <source src={gshit} type="audio/ogg"/>
+  
+  Your browser does not support the audio element.
+</audio>
+
     </div>
   );
 }
